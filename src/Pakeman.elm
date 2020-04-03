4a183613ed91dfa6347cc9667acbd3d7416ee990
kahite <lelievre.yves@gmail.com>
2020-04-03 21:26:33 +0200

    Add time based subscription to simulate event encounters

--- src/Pakeman.elm
+++ src/Pakeman.elm
@@ -3,27 +3,39 @@ module Pakeman exposing (main)
 import Browser
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
+import Task
+import Time
 
 type alias Model = {
         console: List String
     }
 
-type Message = Nothing
+type Message 
+    = AddConsole String
+    | Timer Time.Posix
 
 main : Program () Model Message
 main = Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
 
 init : () -> (Model, Cmd Message)
-init _ = (Model ["test1", "test2"], Cmd.none)
+init _ = (Model ["Welcome to Pakeman !"], Cmd.none)
  
 update : Message -> Model -> (Model, Cmd Message)
 update msg model = 
     case msg of 
-        Nothing ->
-            (model, Cmd.none)
+        AddConsole entry ->
+            ({ model | console = List.append [entry] model.console}, Cmd.none)
+        Timer posix ->
+            (model, cmdAddConsole ("New message " ++ String.fromInt (Time.posixToMillis posix)))
+
+cmdAddConsole: String -> Cmd Message
+cmdAddConsole entry =
+    Task.perform (\_ -> AddConsole entry)  (Task.succeed 1)
+
 
 subscriptions : Model -> Sub Message
-subscriptions model = Sub.none
+subscriptions _ = 
+    Time.every 1000 Timer
 
 
 view : Model -> Html Message
@@ -37,8 +49,7 @@ view model =
         ],
         div [class "w-25"] [
             Html.h3 [] [text "Console"],
-            div [] (List.map (\ elem -> div [] [text elem]) (List.reverse model.console))
-            
+            div [] (List.map (\ elem -> div [] [text elem]) model.console)
         ]
     ]
 
